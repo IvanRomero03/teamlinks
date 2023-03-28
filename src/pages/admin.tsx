4cc3816e5c1c836c96737e61f897380f14ae6903
Gilberto <gilvaldez2003@gmail.com>
2023-03-27 18:51:50 -0600

    admin

---
+++ src/pages/admin.tsx
@@ -0,0 +1,21 @@
+import { type NextPage } from "next";
+import Layout from "y/components/layout/layout";
+
+
+const Admin:NextPage=() =>{
+
+return (
+<Layout>
+
+<h1>Welcome - Admin</h1>
+
+
+
+
+</Layout>
+)
+};
+
+
+
+export default Admin;
