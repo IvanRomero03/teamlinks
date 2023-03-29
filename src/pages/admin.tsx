32f8502d3424760f964badf0fd015966fc768c42
Gilberto <gilvaldez2003@gmail.com>
2023-03-29 17:48:36 -0600

    ...

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,21 +1,17 @@
 import { type NextPage } from "next";
 import Layout from "y/components/layout/layout";
 
-
 const Admin:NextPage=() =>{
-
 return (
 <Layout>
-
 <h1>Welcome - Admin</h1>
-
+<button id="new" className="rounded-md  bg-emerald-400  text-black  m-5" > New entry</button>
+<button id="edit" className="rounded-md bg-emerald-400  text-black  m-5" > edit </button>
 
 
 
 </Layout>
 )
 };
-
-
 
 export default Admin;
