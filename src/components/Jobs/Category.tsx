408191c9ea0436d0d894bf449d764fa2f958c941
IvanRomero03 <i.wells.ar@gmail.com>
2023-04-07 15:34:11 -0600

    jobs page

---
+++ src/components/Jobs/Category.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface Props {
+  name: string;
+  items: string[];
+}
+
+const Category = ({ name, items }: Props) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex flex-col">
+      <p
+        onClick={() => setOpen(!open)}
+        className={
+          "cursor-pointer " +
+          (open ? "text-emerald-400 underline " : " hover:underline")
+        }
+      >
+        {name} {open ? "-" : "+"}
+      </p>
+      {open && (
+        <ul>
+          {items.map((item) => (
+            <li className="ml-4 hover:underline">{item}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Category;
