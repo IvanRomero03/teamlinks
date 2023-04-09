01b178fc4db3e4dcb694f57046ee987e7eb149c1
A00833574 <A00833574@tec.mx>
2023-04-09 15:41:28 -0600

    New Branch

---
+++ src/pages/candidate.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import Layout from "y/components/layout/layout";
+import Demo from "y/components/layout/Demo";
+
+
+const Candidate : NextPage = () => {
+  return (
+    <Layout>
+        <Demo />
+    </Layout>
+  );
+};
+
+export default Candidate;
