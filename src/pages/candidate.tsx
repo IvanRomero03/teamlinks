74ad98031e52cf07582cf2857dc0435f0599b070
A00833574 <A00833574@tec.mx>
2023-04-09 20:12:51 -0600

    Fresh

--- src/pages/candidate.tsx
+++ src/pages/candidate.tsx
@@ -1,14 +1,18 @@
 import { type NextPage } from "next";
 import Layout from "y/components/layout/layout";
-import Demo from "y/components/layout/Demo";
 
-
-const Candidate : NextPage = () => {
-  return (
-    <Layout>
-        <Demo />
-    </Layout>
-  );
+const Candidate: NextPage = () => {
+    return(
+        <Layout
+            Items={[
+                { title: "Home", section: "home" },
+                { title: "About", section: "about" },
+                { title: "My Info", section: "info" },
+                { title: "Settings", section: "settings" },
+            ]}
+        >
+        </Layout>
+    );
 };
 
 export default Candidate;
