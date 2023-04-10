9459cafb19e0f54c3ceb8d7c591d84992b56c9ff
Gilberto <gilvaldez2003@gmail.com>
2023-04-10 17:46:07 -0500

    archivo para leer csv

---
+++ lectura.py
@@ -0,0 +1,34 @@
+print("hello")
+
+class Proyecto():
+    def _init_(self,id,nombre,descripcion,pais,remoto,serviceRegion,estatus,fechaCreacion,fechaEstatusChange,adminId,admin,reclutadorId
+            ,reclutador,numPosicionesTot,numPosicionesDis, puesto,Departamento, departamentoId,Requirement  ):
+        self.id=id 
+        self.nombre=nombre
+        self.descripcion=descripcion
+        self.pais=pais
+        self.remoto=remoto
+        self.serviceRegion=serviceRegion
+        self.estatus=estatus
+        self.fechaCreacion=fechaCreacion
+        self.fechaEstatusChange=fechaEstatusChange
+        self.adminId=adminId
+        self.admin=admin
+        self.reclutadorId=reclutadorId
+        self.reclutador=reclutador
+        self.numPosicionesTot=numPosicionesTot
+        self.numPosicionesDis=numPosicionesDis
+        self.puesto=puesto
+        self.Departamento=Departamento
+        self.departamentoId=departamentoId
+        self.Requirement=Requirement
+        
+
+
+
+#for celda in .csv :
+    #p1=Proyecto(celda k2, #celda...)
+    
+    #mandar p1 a base de datos
+
+
