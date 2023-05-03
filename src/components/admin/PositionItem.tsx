1f24a4f4dae886ae8a87ea93407a2360e91266ba
IvanRomero03 <i.wells.ar@gmail.com>
2023-05-02 23:27:45 -0600

    fix details

--- src/components/admin/PositionItem.tsx
+++ src/components/admin/PositionItem.tsx
@@ -6,7 +6,7 @@ const PositionItem = ({ position }: { position: Puestos }) => {
     <div className="m-2 flex flex-col space-y-4 rounded-md border-white bg-white p-4 text-center shadow-md">
       <div className="flex flex-col justify-between space-y-4">
         <Link
-          href={`/admin/position/${position.id}`}
+          href={`/manager/position/${position.id}`}
           className="text-xl font-bold hover:underline"
         >
           {position.jobTitle}
